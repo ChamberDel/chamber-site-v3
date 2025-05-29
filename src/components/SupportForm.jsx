dd389f30dda7467684282bc884941cca00523a13
ChamberDel <wj.barry80@gmail.com>
2025-05-29 16:02:28 +0100

    29052025

--- src/components/SupportForm.jsx
+++ src/components/SupportForm.jsx
@@ -21,7 +21,7 @@ const SupportForm = () => {
 
     try {
       const response = await fetch(
-        "https://email-server-oru9ych8q-william-barrys-projects.vercel.app/api/send-email",
+        "https://email-server-nine-gules.vercel.app/api/send-email",
         {
           method: "POST",
           headers: {
